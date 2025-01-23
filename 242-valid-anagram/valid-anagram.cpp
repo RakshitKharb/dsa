44a763aa27a54bbaeb0d9bf0704d727a550dd01f
RakshitKharb <123294579+RakshitKharb@users.noreply.github.com>
2025-01-23 13:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (12.49%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int> mpp1;
+        for(auto it:s){
+            mpp1[it]++;
+        }
+        for(auto it:t){
+            mpp1[it]--;
+        }
+        for(auto it:mpp1){
+            if(it.second!=0) return false;
+        }
+        return true;
+    }
+};
