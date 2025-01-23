45c97306cfcf851853d9122236d55349009f172c
RakshitKharb <123294579+RakshitKharb@users.noreply.github.com>
2025-01-23 14:13:35 +0530

    Time: 7 ms (54.16%) | Memory: 13.6 MB (8.49%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -5,10 +5,10 @@ public:
         unordered_map<char,int> mpp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         for(int i=0;i<s.length();i++){
             if(mpp[s[i]]<mpp[s[i+1]]){
-                result=result-mpp[s[i]];
+                result-=mpp[s[i]];
             }
             else{
-                result=result+mpp[s[i]];
+                result+=mpp[s[i]];
             }
         }
         return result;
