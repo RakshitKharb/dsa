3ed63daeab2221277b120a0c208d090745546d7d
RakshitKharb <123294579+RakshitKharb@users.noreply.github.com>
2025-02-07 21:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.74%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,19 +3,17 @@ public:
     double myPow(double x, int n) {
         double ans=1;
         long long nn=n;
-        
         if(n<0){
             nn=-nn;
         }
-        
         while(nn>0){
-            if(nn%2==1){
-                ans=ans*x;
-                nn=nn-1;
-            }
-            else{
+            if(nn%2==0){
                 x=x*x;
                 nn=nn/2;
+            }
+            else{
+                ans=ans*x;
+                nn=nn-1;
             }
         }
         if(n<0){
