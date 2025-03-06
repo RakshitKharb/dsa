95230ebf141e1b7ec5ecbc60504018088c0b811d
RakshitKharb <123294579+RakshitKharb@users.noreply.github.com>
2025-03-06 19:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.75%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -5,8 +5,8 @@ public:
         int j=n-1;
         int k=m+n-1;
 
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
             }
@@ -16,10 +16,6 @@ public:
             }
             k--;
         }
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
+       
     }
 };
